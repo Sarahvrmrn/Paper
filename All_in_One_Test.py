1df4233893b15504364272ec02e554226f834e57
Sarahvrmrn <sarah.vermeeren@h-brs.de>
2024-06-24 15:16:16 +0200

    peak picking added

--- All_in_One_Test.py
+++ All_in_One_Test.py
@@ -40,6 +40,7 @@ def read_files(path: str, tag: str):
     files = hp.get_all_files_in_dir_and_sub(path)
     files = [f for f in files if f.find('.csv') >= 0]
     merged_df = pd.DataFrame()
+    merged_peak_df = pd.DataFrame()
     info = []
 
     for file in files:
@@ -47,15 +48,15 @@ def read_files(path: str, tag: str):
         x = df['RT(milliseconds)']
         y = df['TIC']
         y = hp.smooth_spectrum(y)
-        y = hp.baseline_correction(y)
-        y = hp.area_normalization(x,y)
-
-      
-        x_values = df['RT(milliseconds)']
-        y_values = df['TIC'].rolling(window=25).mean()
-        df = pd.DataFrame({'RT(milliseconds)': x_values, 'TIC': y_values})
-        
-        
+        baseline = hp.baseline_correction(y)
+        y_corrected = y- baseline
+        y = hp.area_normalization(x,y_corrected)
+        baseline_y_area = hp.baseline_correction(y)*0.05
+    
+        # x_values = df['RT(milliseconds)']
+        # y_values = df['TIC'].rolling(window=10).mean()
+        # df = pd.DataFrame({'RT(milliseconds)': x_values, 'TIC': y_values})
+           
         df.set_index('RT(milliseconds)', inplace=True)
         new_index = np.arange(120000, 823100, 100)
         df = df.reindex(new_index)
@@ -66,24 +67,46 @@ def read_files(path: str, tag: str):
 
         info.append(
             {'Class': file.split('\\')[5], 'filename': os.path.basename(file)})
+        
+        peaks = hp.pick_peaks(y)
+        
+        peak_areas = hp.integrate_spectrum(x, y, peaks) 
+        y_corr_bl = baseline_y_area[peaks]  
+        x_peaks = x[peaks] 
+    # integrate picked peaks
+     # define list of x-values for each peak
+        peak_df = pd.DataFrame({'Peak Position Index': peaks, 'RT(milliseconds)': x_peaks, 'Peak Area': peak_areas, 'Corrected baseline': y_corr_bl})
+        peak_df = peak_df.drop(['Peak Position Index', 'Corrected baseline'], axis=1)
 
+        peak_df.set_index('RT(milliseconds)', inplace=True)
+        merged_peak_df = pd.merge(merged_peak_df, peak_df, how='outer', left_index=True, right_index=True)
+        merged_peak_df = merged_peak_df.fillna(5)
+        merged_peak_df = merged_peak_df.rename(columns={'Peak Area': file.split('\\')[5]})
+        
+        
     merged_df.drop(merged_df.index[6601:], inplace=True)
     merged_df.drop(merged_df.index[:600], inplace=True)
 
     df_info = pd.DataFrame(info)
     
-    threshold_percent = 0 # threshold in %
+    threshold_percent = 0.1 # threshold in %
     
     max_value = merged_df.max().max()
     threshold = max_value * (threshold_percent / 100)
     
     merged_df[merged_df <= threshold] = 0
-    
+
     
     hp.save_df(merged_df, join(
         os.environ["ROOT_PATH"], 'data'), f'extracted_features_{tag}')
     hp.save_df(df_info, join(
         os.environ["ROOT_PATH"], 'data'), f'extracted_features_info_{tag}')
+    
+    hp.save_df(merged_peak_df, join(
+        os.environ["ROOT_PATH"], 'data'), f'extracted_peaks_{tag}')
+    hp.save_df(df_info, join(
+        os.environ["ROOT_PATH"], 'data'), f'extracted_peaks_info_{tag}')
+   
    
     
 
@@ -237,13 +260,24 @@ if __name__ == '__main__':
          os.environ["ROOT_PATH"], 'data', f'extracted_features_info_test.csv'))
     
     # Combine both LDA DataFrames
+    lda, df_lda_train_peaks, df_info = create_lda(join(
+         os.environ["ROOT_PATH"], 'data', f'extracted_peaks_train.csv'),
+         join(
+         os.environ["ROOT_PATH"], 'data', f'extracted_peaks_info_train.csv'))
+    
+    df_lda_test_peaks, predictions = push_to_lda(lda, join(
+         os.environ["ROOT_PATH"], 'data', f'extracted_peaks_test.csv'),
+         join(
+         os.environ["ROOT_PATH"], 'data', f'extracted_peaks_info_test.csv'))
+    
+    merged_df_peaks = combine_data(df_lda_train_peaks,df_lda_test_peaks)
     
     merged_df = combine_data(df_lda_test, df_lda_train)
     
     # Plot LDA
     
     plot(merged_df)
-    
+    plot(merged_df_peaks)
     # combine both PCA DataFrames
     
     # merged_pc = combine_data(transformded_data_test, df_pca)
---
+++ retention_shift.py
@@ -0,0 +1,58 @@
+# import modules
+import os               # for interaction with operating system, change/select directories, etc. https://hellocoding.de/blog/coding-language/python/os-module-richtig-sicher-nutzen
+from os.path import join
+import numpy as np      # for numerical operations
+import pandas as pd     # for data cleaning and analysis
+import seaborn as sns   # part of numpy, for distributions
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+#import custom functions
+from helpers import Helpers as hp  # supporting file with additional, reoccuring functions
+
+def shift_retentiontime(input_dir, shift_value, output_csv_dir):
+    # Ensure the output directory exists
+    os.makedirs(output_csv_dir, exist_ok=True)
+
+    # Iterate over all CSV files in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".csv"):
+            input_csv_path = os.path.join(input_dir, filename)
+            
+            # Read the CSV file
+            df = pd.read_csv(input_csv_path)
+    
+        # Check if the first column contains numeric values from the first row onwards
+        if pd.api.types.is_numeric_dtype(df.iloc[0:, 0]):
+            # Add the shift_value to every value in the first column starting from the first row
+            df.iloc[0:, 0] += shift_value
+        else:
+            print("The first column does not contain numeric values from the first row onwards.")
+            continue
+    
+        # Get the original file name without the extension
+        original_file_name = os.path.splitext(filename)[0]
+
+        # Determine if the add_value is positive or negative and set the descriptor
+        descriptor = "plus" if shift_value >= 0 else "minus"
+    
+        # Create the absolute value for the output file name
+        abs_shift_value = abs(shift_value)
+
+        # Generate the output file name by appending the shift_value to the original file name
+        output_csv_name = f'{original_file_name}_{descriptor}_{abs_shift_value}.csv'
+        output_csv_path = os.path.join(output_csv_dir, output_csv_name)
+    
+        # Save the modified dataframe to a new CSV file
+        df.to_csv(output_csv_path, index=False)
+        print(f"Modified CSV saved to {output_csv_path}")
+
+# select input and output directories
+input_dir = 'C:\\Users\\sverme-adm\\Desktop\\Paper_Test'
+output_csv_dir = 'C:\\Users\\sverme-adm\\Desktop\\Data_Sorten'
+
+shift_value = 300      # shift the retention time (first column) by this value 
+
+# execute function
+shift_retentiontime(input_dir, shift_value, output_csv_dir)
